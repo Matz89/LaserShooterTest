3643d1563f83b3aafdce601935361eb360b92d64
Kait-PC\Kait <matthewbpelletier@gmail.com>
2017-01-16 21:16:24 -0500

    Created Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		Vector2 move = new Vector2(0.0f, 0.0f);
+		if(Input.GetKey(KeyCode.LeftArrow))
+			move.x--;
+		if(Input.GetKey(KeyCode.RightArrow))
+			move.x++;
+
+		GetComponent<Rigidbody2D>().velocity = move * speed;
+	}
+}
