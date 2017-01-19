e62e44424a492c3127602525478b492336d4f442
Kait-PC\Kait <matthewbpelletier@gmail.com>
2017-01-18 21:19:36 -0500

    Lasers moving, being destroyed.  Enemies have garbage AI. etc.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	public bool goRight = true;
+	public float speed = 0.05f;
+
+	private float xmin;
+	private float xmax;
+	private float padding;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint( new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint( new Vector3(1,0,distance));
+		padding = 0f;
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+		
+
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
+	}
+
+	public void OnDrawGizmos ()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (goRight) {
+			//move
+			transform.position += Vector3.right * speed * Time.deltaTime;
+
+			//switch direction
+			if((transform.position.x + padding) >= xmax)
+				goRight = false;
+		} else {
+			//move
+			transform.position -= Vector3.right * speed * Time.deltaTime;
+
+			//switch direction
+			if((transform.position.x - padding) <= xmin)
+				goRight = true;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		
+
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public float padding = 1f;
+	public GameObject projectile;
+	public float projectileSpeed = 1.0f;
+	public float firingRate = 0.5f;
+
+	float xmin;
+	float xmax;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint( new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint( new Vector3(1,0,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
 	}
 	
 	// Update is called once per frame
@@ -16,13 +28,35 @@ public class PlayerController : MonoBehaviour {
 		
 	}
 
-	void FixedUpdate(){
-		Vector2 move = new Vector2(0.0f, 0.0f);
-		if(Input.GetKey(KeyCode.LeftArrow))
+	void FixedUpdate ()
+	{
+		Vector2 move = new Vector2 (0.0f, 0.0f);
+		if (Input.GetKey (KeyCode.LeftArrow))
 			move.x--;
-		if(Input.GetKey(KeyCode.RightArrow))
+		if (Input.GetKey (KeyCode.RightArrow))
 			move.x++;
 
-		GetComponent<Rigidbody2D>().velocity = move * speed;
+		GetComponent<Rigidbody2D> ().velocity = move * speed;
+
+		/*Clamp distance player can move*/
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector2 (newX, transform.position.y);
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			InvokeRepeating("Fire",0.0001f, firingRate);
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space)){
+			CancelInvoke();
+		}
+
+
+	}
+
+	//shooting laser
+	void Fire(){
+		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = Vector2.up * projectileSpeed;
+		
 	}
 }
